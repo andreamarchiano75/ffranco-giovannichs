681194ec780108d7f2d4f9ddc37f8e4e120edfdf
andreamarchiano75 <124669190+andreamarchiano75@users.noreply.github.com>
2023-02-07 12:44:12 +0100

    Add files via upload

---
+++ pane.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main()
+{
+  
+    int a;
+    int b;
+    printf("inserisci due valori\n");
+    scanf("%d %d", &a, &b);
+    
+    if (a > b) 
+    {
+        printf("a è maggiore di b");
+    }
+
+    else if (a == b)
+    {
+        printf("uguale logico");
+    }
+
+    if (a < b)
+    {
+        printf("a è minore di b");
+    }
+
+
+
+
+
+
+
+
+
+}
+
+
+
