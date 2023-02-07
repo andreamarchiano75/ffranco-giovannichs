7d126857f8a6cab05e4d40896d32fc3fa0aa4815
andreamarchiano75 <124669190+andreamarchiano75@users.noreply.github.com>
2023-02-07 10:27:21 +0100

    Add files via upload

---
+++ mari.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main ()
+{
+    int x;
+    int y;
+
+printf ("x era %d\n", x);
+printf ("y era %d\n", y);
+printf ("inserisci due numeri\n");
+scanf ("%d %d", &x, &y);
+printf ("x diventa: %d\n", x);
+printf ("y diventa: %d\n", y);
+}
---
+++ marina.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main ()
+{
+    char c1 = 109;
+    char c2 = 'a';
+    char c3 = 114;
+    char c4 = 'i';
+
+    printf ("%c\n", c1);
+    printf ("%c\n", c2);
+    printf ("%c\n", c3);
+    printf ("%c\n", c4);
+}
