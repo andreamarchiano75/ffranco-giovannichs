c194275fe83c9514e8573d6a363b670c847c815e
andreamarchiano75 <124669190+andreamarchiano75@users.noreply.github.com>
2023-02-07 16:59:29 +0100

    Add files via upload

---
+++ numero.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    int b;
+    int c;
+
+    printf("inserisci tre numeri");
+    scanf("%d %d %d", &a, &b, &c);
+
+    if(b - a == c - b)
+    {
+        printf("pr");
+    }
+    else
+    {
+        printf("n");
+    }
+   
+
+}
